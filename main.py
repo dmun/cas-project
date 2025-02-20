6385427e928f8ccd97cee5d3494e090b9c54ba91
David van Munster <davidru01@live.nl>
2025-02-20 22:37:21 +0100

    add initial snake game

--- main.py
+++ main.py
@@ -1,123 +1,103 @@
-import jax
-import jax.numpy as jnp
-from jax import random
-import numpy as np
 import pyray as rl
-
-GRAVITY = 9.8
-num_particles = 10000
-
-key = random.PRNGKey(0)
-positions = random.uniform(key, (num_particles, 2), minval=0, maxval=600)
-velocities = random.uniform(key, (num_particles, 2), minval=-2, maxval=2)
-forces = random.uniform(key, (num_particles, 2), minval=-2, maxval=2)
-masses = random.uniform(key, (num_particles, 2), minval=0, maxval=10)
-
-
-@jax.jit
-def update_positions(
-    positions: jax.Array,
-    masses: jax.Array,
-    forces: jax.Array,
-    velocities: jax.Array,
-    dt: float,
-    mouse_pos: jax.Array,
-):
-    direction = mouse_pos - positions
-    distance = jnp.linalg.norm(direction, axis=1, keepdims=True)
-    direction = direction / (distance + 1e-8)  # Normalize and avoid division by zero
-    forces = direction * GRAVITY * masses
-    velocities += forces / masses * dt
-    positions += velocities * dt
-
-    # Collision detection with window boundaries
-    positions = jnp.where(positions <= 0, 0, positions)
-    positions = jnp.where(
-        positions >= jnp.array([WIDTH, HEIGHT]),
-        jnp.array([WIDTH, HEIGHT]),
-        positions,
-    )
-    velocities = jnp.where(positions <= 0, 0, velocities)
-    velocities = jnp.where(
-        positions >= jnp.array([WIDTH, HEIGHT]),
-        0,
-        velocities,
-    )
-    colors = jnp.clip(jnp.abs(velocities.sum(axis=1)) * 2, 0, 255)
-
-    return positions, velocities, colors
-
+import numpy as np
+from enum import Enum
+from pyray import KeyboardKey
 
 WIDTH = 800
 HEIGHT = 600
-rl.init_window(WIDTH, HEIGHT, "Simulation")
-rl.set_target_fps(120)
+SNAKE_SIZE = 20
+SNAKE_SPEED = 20
+CELLS = 20
+CELL_SIZE = int(HEIGHT / CELLS)
 
-mouse_pos = rl.get_mouse_position()
+rl.init_window(WIDTH, HEIGHT, "Snake Game")
+rl.set_target_fps(60)
 
-shader = rl.load_shader("shader.vert.glsl", "shader.frag.glsl")
-positions_loc = rl.get_shader_location(shader, "positions")
 
-camera = rl.Camera3D()
-camera.position = rl.Vector3(0.0, 5.0, 1.0)
-camera.target = rl.Vector3(0.0, 0.0, 0.0)
-camera.up = rl.Vector3(0.0, 1.0, 0.0)
-camera.fovy = 90.0
-camera.projection = rl.CameraProjection.CAMERA_PERSPECTIVE
+class Direction(Enum):
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
 
-vertices = np.zeros((num_particles, 2), dtype=np.float32).flatten()
-vertex_buffer = rl.rl_load_vertex_buffer(
-    rl.ffi.new("float[1000]"),
-    num_particles,
-    True,
-)
-# rl.rl_load_vertex_buffer()
 
+class Snake:
+    length = 8
+    body = [(8, 8)]
+    dir = Direction.RIGHT
+
+    def resolve_direction(self, new_dir: Direction):
+        dir = self.dir
+        if new_dir.value[0] != 0 and dir.value[1] != 0:
+            dir = new_dir
+        elif new_dir.value[1] != 0 and dir.value[0] != 0:
+            dir = new_dir
+        self.dir = dir
+
+
+grid = np.zeros((CELLS, CELLS), dtype=np.int8)
+grid[3, 3] = -1
+
+snake = Snake()
+
+last = 0
+steps = 0
+new_dir = Direction.RIGHT
 while not rl.window_should_close():
-    dt = rl.get_frame_time() * 2
-    mouse = rl.get_mouse_position()
-    mouse_pos = jnp.array([mouse.x, mouse.y])
-    positions, velocities, colors = update_positions(
-        positions,
-        masses,
-        forces,
-        velocities,
-        dt,
-        mouse_pos,
-    )
+    match rl.get_key_pressed():
+        case KeyboardKey.KEY_K:
+            new_dir = Direction.UP
+        case KeyboardKey.KEY_J:
+            new_dir = Direction.DOWN
+        case KeyboardKey.KEY_H:
+            new_dir = Direction.LEFT
+        case KeyboardKey.KEY_L:
+            new_dir = Direction.RIGHT
 
-    # positions_flat = np.array(positions).flatten().astype(np.float32).tolist()
-    # model = rl.upload_mesh(positions_flat, False)
-    # rl.set_shader_value_v(
-    #     shader,
-    #     positions_loc,
-    #     rl.ffi.new("float[]", positions_flat),
-    #     rl.ShaderUniformDataType.SHADER_UNIFORM_VEC2,
-    #     num_particles,
-    # )
+    dt = rl.get_frame_time()
+    last += dt
+    if last > 0.100:
+        step = True
+        last = 0
+    else:
+        step = False
+
+    if step:
+        steps += 1
+        snake.resolve_direction(new_dir)
+        new_head = (snake.body[0][0] + snake.dir.value[0], snake.body[0][1] + snake.dir.value[1])
+        snake.body.insert(0, new_head)
+        if len(snake.body) > snake.length:
+            tail = snake.body.pop()
+            grid[tail] = 0
+
+        if new_head[0] < 0 or new_head[0] >= CELLS or new_head[1] < 0 or new_head[1] >= CELLS:
+            break
+
+        for i, segment in enumerate(snake.body):
+            grid[segment] = snake.length - i
 
     rl.begin_drawing()
-    # rl.begin_mode_3d(camera)
-    rl.clear_background(rl.BLACK)
-    for pos, color in zip(np.array(positions), np.array(colors)):
-        rl.draw_rectangle(
-            pos[0],
-            pos[1],
-            1,
-            1,
-            (color, 155, 255 - color, 128),
-        )
-        # rl.draw_point_3d((pos[0], pos[1], 0), rl.RED)
+    rl.clear_background(rl.color_from_hsv(0, 0, 0.1))
+    rl.draw_grid(CELLS, CELL_SIZE)
+    for i, rows in enumerate(grid):
+        for j, cell in enumerate(rows):
+            if cell == -1:
+                color = rl.GREEN
+            elif cell >= 1:
+                color = rl.color_from_hsv(55, 1, cell / snake.length)
+            else:
+                color = rl.color_from_hsv(0, 1, cell / snake.length)
 
-    # rl.rl_enable_point_mode()
-    # rl.begin_shader_mode(shader)
-    # rl.rl_draw_vertex_array_elements(
-    #     0, num_particles, vertex_buffer
-    # )
-    # rl.end_shader_mode()
+            rl.draw_rectangle(
+                j * CELL_SIZE,
+                i * CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE,
+                color,
+            )
 
-    # rl.end_mode_3d()
-    rl.draw_fps(0, 0)
+    rl.draw_text(f"{steps} steps", HEIGHT + 8, 8, 24, rl.RAYWHITE)
     rl.end_drawing()
 
 rl.close_window()
